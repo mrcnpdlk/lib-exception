159747cd0c8b8db97a26236aa00559666ce209ff
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-30 20:11:27 +0100

    first commit

---
+++ src/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 30.03.2019
+ * Time: 20:10
+ */
+
+namespace Mrcnpdlk\Lib;
+
+/**
+ * Class Exception
+ *
+ * @package Mrcnpdlk\Lib
+ */
+class Exception extends \Exception
+{
+
+}
